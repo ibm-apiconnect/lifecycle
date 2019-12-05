76ae2fa404a3e37c16a9321ae6c05f6d3a40a168
Shane Claussen <claussen@us.ibm.com>
2019-12-04 17:25:03 -0800

    Changed env vars from - to _

--- scripts/create-provider-organization.sh
+++ scripts/create-provider-organization.sh
@@ -8,9 +8,9 @@ source ./env.sh
 echo Authenticate as the admin user
 response=`curl -X POST https://${management}/api/token \
                -s -k -H "Content-Type: application/json" -H "Accept: application/json" \
-               -d "{ \"realm\": \"${admin-idp}\",
+               -d "{ \"realm\": \"${admin_idp}\",
                      \"username\": \"admin\",
-                     \"password\": \"${admin-password}\",
+                     \"password\": \"${admin_password}\",
                      \"client_id\": \"599b7aef-8841-4ee2-88a0-84d49c4d6ff2\",
                      \"client_secret\": \"0ea28423-e73b-47d4-b40e-ddb45c48bb0c\",
                      \"grant_type\": \"password\" }"`
@@ -20,16 +20,16 @@ export token=`echo ${response} | jq -r '.access_token'`
 
 
 echo Create the Provider Organization Owner
-response=`curl https://${management}/api/user-registries/admin/${provider-user-registry}/users \
-               -s -k -H "Content-Type:application/json" -H "Accept:application/json" \
+response=`curl https://${management}/api/user-registries/admin/${provider_user_registry}/users \
+               -s -k -H "Content-Type: application/json" -H "Accept: application/json" \
                -H "Authorization: Bearer ${token}" \
-               -d "{ \"username\": \"${provider-username}\",
-                     \"password\": \"${provider-password}\",
-                     \"email\": \"${provider-email}\",
-                     \"first_name\": \"${provider-firstname}\",
-                     \"last_name\": \"${provider-lastname}\" }"`
+               -d "{ \"username\": \"${provider_username}\",
+                     \"password\": \"${provider_password}\",
+                     \"email\": \"${provider_email}\",
+                     \"first_name\": \"${provider_firstname}\",
+                     \"last_name\": \"${provider_lastname}\" }"`
 echo ${response} | jq .
-export user-url=`echo ${response} | jq -r '.url'`
+export user_url=`echo ${response} | jq _r '.url'`
 
 
 
@@ -37,17 +37,17 @@ echo Create the Provider Organization
 response=`curl https://${management}/api/cloud/orgs \
                -s -k -H "Content-Type: application/json" -H "Accept: application/json" \
                -H "Authorization: Bearer ${token}" \
-               -d "{ \"name\": \"${porg-name}\",
-                     \"title\": \"${porg-title}\",
+               -d "{ \"name\": \"${porg_name}\",
+                     \"title\": \"${porg_title}\",
                      \"org_type\": \"provider\",
-                     \"owner_url\": \"${owner-url}\" }"`
+                     \"owner_url\": \"${owner_url}\" }"`
 echo ${response} | jq .
-export owner-url=`echo ${response} | jq -r '.url'`
+export owner_url=`echo ${response} | jq -r '.url'`
 
 
 
 echo Get the Provider Organization Owner
-response=`curl -X GET ${owner-url} \
+response=`curl -X GET ${owner_url} \
                -s -k -H "Accept: application/json" \
                -H "Authorization: Bearer ${token}"`
 echo ${response} | jq .
@@ -55,7 +55,7 @@ echo ${response} | jq .
 
 
 echo Get the Provider Organization
-response=`curl -X GET ${porg-url} \
+response=`curl -X GET ${porg_url} \
                -s -k -H "Accept: application/json" \
                -H "Authorization: Bearer ${token}"`
 echo ${response} | jq .
@@ -63,7 +63,7 @@ echo ${response} | jq .
 
 
 echo Delete the Provider Organization
-response=`curl -X DELETE ${porg-url} \
+response=`curl -X DELETE ${porg_url} \
                -s -k -H "Accept: application/json" \
                -H "Authorization: Bearer ${token}"`
 echo ${response} | jq .
@@ -71,7 +71,7 @@ echo ${response} | jq .
 
 
 echo Delete the Provider Organization Owner
-response=`curl -X DELETE ${owner-url}
+response=`curl -X DELETE ${owner_url}
                -s -k -H "Accept: application/json" \
                -H "Authorization: Bearer ${token}"`
 echo ${response} | jq .
--- scripts/env.sh
+++ scripts/env.sh
@@ -1,16 +1,16 @@
 export management=api.admin-201841-1202.loki.dev.ciondemand.com
 
-export admin-idp=admin/default-idp-1
-export admin-password=${admin-password:-8iron-hide}
+export admin_idp=admin/default-idp-1
+export admin_password=${admin-password:-8iron-hide}
 
-export provider-user-registry=api-manager-lur
-export provider-idp=provider/default-idp-2
+export provider_user_registry=api-manager-lur
+export provider_idp=provider/default-idp-2
 
-export provider-username=steve
-export provider-password=${provider-password:-8iron-hide}
-export provider-email=steve@acme.com
-export provider-firstname=steve
-export provider-lastname=owner
+export provider_username=steve
+export provider_password=${provider-password:-8iron-hide}
+export provider_email=steve@acme.com
+export provider_firstname=steve
+export provider_lastname=owner
 
-export porg-name=acme
-export porg-title=acme
+export porg_name=acme
+export porg_title=acme
