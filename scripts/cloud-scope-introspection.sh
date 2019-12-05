b890bf569b4fefdfa1174c0c0f90d2544f52e063
Shane Claussen <claussen@us.ibm.com>
2019-12-05 09:45:27 -0800

    Tested cloud-scope-introspectionsh

--- scripts/cloud-scope-introspection.sh
+++ scripts/cloud-scope-introspection.sh
@@ -5,6 +5,7 @@ source ./env.sh
 
 
 
+echo
 echo Authenticate as the admin user
 response=`curl -X POST https://${management}/api/token \
                -s -k -H "Content-Type: application/json" -H "Accept: application/json" \
@@ -19,6 +20,7 @@ export token=`echo ${response} | jq -r '.access_token'`
 
 
 
+echo
 echo Get the Cloud Scope Settings
 response=`curl -X GET https://${management}/api/cloud/settings \
                -s -k -H "Accept: application/json" \
@@ -27,6 +29,7 @@ echo ${response} | jq .
 
 
 
+echo
 echo Get the Cloud Scope Mail Server Configured Setting
 response=`curl -X GET https://${management}/api/cloud/settings/mail-server-configured \
                -s -k -H "Accept: application/json" \
@@ -35,14 +38,7 @@ echo ${response} | jq .
 
 
 
-echo Get the Cloud Scope Mail Server Configured Setting
-response=`curl -X GET https://${management}/api/cloud/settings/mail-server-configured \
-               -s -k -H "Accept: application/json" \
-               -H "Authorization: Bearer ${token}"`
-echo ${response} | jq .
-
-
-
+echo
 echo Get the Cloud Scope User Registries Setting
 response=`curl -X GET https://${management}/api/cloud/settings/user-registries \
                -s -k -H "Accept: application/json" \
@@ -51,62 +47,70 @@ echo ${response} | jq .
 
 
 
+echo
 echo Get the Cloud Scope Notification Templates
 response=`curl -X GET https://${management}/api/cloud/settings/notification-templates \
                -s -k -H "Accept: application/json" \
                -H "Authorization: Bearer ${token}"`
-echo ${response} | jq .
+echo ${response}
 
 
 
+echo
 echo Get the Cloud Scope Notification Templates Cloud
 response=`curl -X GET https://${management}/api/cloud/settings/notification-templates/cloud \
                -s -k -H "Accept: application/json" \
                -H "Authorization: Bearer ${token}"`
-echo ${response} | jq .
+echo ${response}
 
 
 
+echo
 echo Get the Cloud Scope Notification Templates Admin
 response=`curl -X GET https://${management}/api/cloud/settings/notification-templates/admin \
                -s -k -H "Accept: application/json" \
                -H "Authorization: Bearer ${token}"`
-echo ${response} | jq .
+echo ${response}
 
 
 
+echo
 echo Get the Cloud Scope Notification Templates Provider
 response=`curl -X GET https://${management}/api/cloud/settings/notification-templates/provider \
                -s -k -H "Accept: application/json" \
                -H "Authorization: Bearer ${token}"`
-echo ${response} | jq .
+echo ${response}
 
 
 
+echo
 echo Get the Cloud Scope Notification Templates Catalog
 response=`curl -X GET https://${management}/api/cloud/settings/notification-templates/catalog \
                -s -k -H "Accept: application/json" \
                -H "Authorization: Bearer ${token}"`
-echo ${response} | jq .
+echo ${response}
 
 
 
+echo
 echo Get the Cloud Scope Notification Templates Space
 response=`curl -X GET https://${management}/api/cloud/settings/notification-templates/space \
                -s -k -H "Accept: application/json" \
                -H "Authorization: Bearer ${token}"`
-echo ${response} | jq .
+echo ${response}
 
 
 
+echo
 echo Get the Cloud Scope Notification Templates Consumer
 response=`curl -X GET https://${management}/api/cloud/settings/notification-templates/consumer \
                -s -k -H "Accept: application/json" \
                -H "Authorization: Bearer ${token}"`
-echo ${response} | jq .
+echo ${response}
 
 
 
+echo
 echo Get the Cloud Scope Settings Role Defaults
 response=`curl -X GET https://${management}/api/cloud/settings/role-defaults \
                -s -k -H "Accept: application/json" \
@@ -115,6 +119,7 @@ echo ${response} | jq .
 
 
 
+echo
 echo Get the Cloud Scope Settings Role Defaults Provider
 response=`curl -X GET https://${management}/api/cloud/settings/role-defaults/provider \
                -s -k -H "Accept: application/json" \
@@ -123,6 +128,7 @@ echo ${response} | jq .
 
 
 
+echo
 echo Get the Cloud Scope Settings Role Defaults Consumer
 response=`curl -X GET https://${management}/api/cloud/settings/role-defaults/consumer \
                -s -k -H "Accept: application/json" \
@@ -131,6 +137,7 @@ echo ${response} | jq .
 
 
 
+echo
 echo Get the Cloud Scope Topology
 response=`curl -X GET https://${management}/api/cloud/topology \
                -s -k -H "Accept: application/json" \
@@ -138,7 +145,7 @@ response=`curl -X GET https://${management}/api/cloud/topology \
 echo ${response} | jq .
 
 
-
+echo
 echo Get the Cloud Scope Admin Identity Providers
 response=`curl -X GET https://${management}/api/cloud/admin/identity-providers \
                -s -k -H "Accept: application/json" \
@@ -147,6 +154,7 @@ echo ${response} | jq .
 
 
 
+echo
 echo Get the Cloud Scope Provider Identity Providers
 response=`curl -X GET https://${management}/api/cloud/provider/identity-providers \
                -s -k -H "Accept: application/json" \
@@ -155,6 +163,7 @@ echo ${response} | jq .
 
 
 
+echo
 echo Get the Cloud Scope Integrations
 response=`curl -X GET https://${management}/api/cloud/integrations \
                -s -k -H "Accept: application/json" \
@@ -163,6 +172,7 @@ echo ${response} | jq .
 
 
 
+echo
 echo Get the Cloud Scope Integrations Billing
 response=`curl -X GET https://${management}/api/cloud/integrations/billing \
                -s -k -H "Accept: application/json" \
@@ -171,6 +181,7 @@ echo ${response} | jq .
 
 
 
+echo
 echo Get the Cloud Scope Integrations Payment Method
 response=`curl -X GET https://${management}/api/cloud/integrations/payment-method \
                -s -k -H "Accept: application/json" \
@@ -179,6 +190,7 @@ echo ${response} | jq .
 
 
 
+echo
 echo Get the Cloud Scope Integrations User Registry
 response=`curl -X GET https://${management}/api/cloud/integrations/user-registry \
                -s -k -H "Accept: application/json" \
@@ -187,6 +199,7 @@ echo ${response} | jq .
 
 
 
+echo
 echo Get the Cloud Scope Integrations Gateway Service
 response=`curl -X GET https://${management}/api/cloud/integrations/gateway-service \
                -s -k -H "Accept: application/json" \
@@ -195,6 +208,7 @@ echo ${response} | jq .
 
 
 
+echo
 echo Get the Cloud Scope Registrations
 response=`curl -X GET https://${management}/api/cloud/registrations \
                -s -k -H "Accept: application/json" \
@@ -203,6 +217,7 @@ echo ${response} | jq .
 
 
 
+echo
 echo Get the Cloud Scope Permissions
 response=`curl -X GET https://${management}/api/cloud/permissions \
                -s -k -H "Accept: application/json" \
@@ -211,6 +226,7 @@ echo ${response} | jq .
 
 
 
+echo
 echo Get the Cloud Scope Permissions Cloud
 response=`curl -X GET https://${management}/api/cloud/permissions/cloud \
                -s -k -H "Accept: application/json" \
@@ -219,6 +235,7 @@ echo ${response} | jq .
 
 
 
+echo
 echo Get the Cloud Scope Permissions Organization
 response=`curl -X GET https://${management}/api/cloud/permissions/org \
                -s -k -H "Accept: application/json" \
@@ -227,6 +244,7 @@ echo ${response} | jq .
 
 
 
+echo
 echo Get the Cloud Scope Permissions Provider
 response=`curl -X GET https://${management}/api/cloud/permissions/provider \
                -s -k -H "Accept: application/json" \
@@ -235,6 +253,7 @@ echo ${response} | jq .
 
 
 
+echo
 echo Get the Cloud Scope Permissions Deployment
 response=`curl -X GET https://${management}/api/cloud/permissions/deployment \
                -s -k -H "Accept: application/json" \
@@ -243,6 +262,7 @@ echo ${response} | jq .
 
 
 
+echo
 echo Get the Cloud Scope Permissions Consumer
 response=`curl -X GET https://${management}/api/cloud/permissions/consumer \
                -s -k -H "Accept: application/json" \
@@ -251,6 +271,7 @@ echo ${response} | jq .
 
 
 
+echo
 echo Get the Cloud Scope Provider Organization Invitations
 response=`curl -X GET https://${management}/api/cloud/org-invitations \
                -s -k -H "Accept: application/json" \
@@ -259,6 +280,7 @@ echo ${response} | jq .
 
 
 
+echo
 echo Get the Cloud Scope Organizations
 response=`curl -X GET https://${management}/api/cloud/orgs \
                -s -k -H "Accept: application/json" \
@@ -267,6 +289,7 @@ echo ${response} | jq .
 
 
 
+echo
 echo Get the Cloud Scope Provider Groups
 response=`curl -X GET https://${management}/api/cloud/provider/groups \
                -s -k -H "Accept: application/json" \
@@ -275,6 +298,7 @@ echo ${response} | jq .
 
 
 
+echo
 echo Get the Cloud Scope Webhooks
 response=`curl -X GET https://${management}/api/cloud/webhooks \
                -s -k -H "Accept: application/json" \
@@ -283,6 +307,7 @@ echo ${response} | jq .
 
 
 
+echo
 echo Get the Cloud Scope Primary Events
 response=`curl -X GET https://${management}/api/cloud/primary-events \
                -s -k -H "Accept: application/json" \
