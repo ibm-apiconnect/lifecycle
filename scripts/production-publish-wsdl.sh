ed2f47c1dce88c38d65b1ae86f4cf2e06a377a64
Shane Claussen <claussen@us.ibm.com>
2019-12-12 13:30:57 -0800

    Updated/tested publish scripts

--- scripts/production-publish-wsdl.sh
+++ scripts/production-publish-wsdl.sh
@@ -6,16 +6,16 @@
 
 
 # Overview
-# - ...
+# - Publish a product that generates an OpenAPI proxy from WSDL
 
 
 
 # Environment variables specific to this script
 export management=${management}
 export provider_idp=${provider_idp}
-export provider_username=${provider_username}
+export provider_username=steve-production
 export provider_password=${provider_password}
-export porg=${porg}
+export porg=acme-production
 export catalog_prod=${catalog_prod}
 export space_travel=${space_travel}
 
@@ -31,8 +31,10 @@ mv calculator.yaml calculator100-api.yaml
 
 echo
 echo Create the Math Product referencing the Calculator API
-rm -f math100-product.yaml
-apic create:product --name "math" --version "1.0.0" --title "Math Product" --apis "calculator100-api.yaml" --filename math100-product.yaml
+# rm -f math100-product.yaml
+# apic create:product --name "math" --version "1.0.0" --title "Math Product" --apis "calculator100-api.yaml" --filename math100-product.yaml
+# apic create:product --name "math" --version "1.0.0" --title "Math Product" --apis "calculator100-api.yaml" --gateway-type "datapower-api-gateway" --filename math100-product.yaml
+# Once created, the $ref reference to the API file must be replace with name: api-name:api-version (see math100-product.yaml)
 
 
 
@@ -53,10 +55,12 @@ export provider_token=`echo ${response} | jq -r '.access_token'`
 
 echo
 echo Publish the Product to the Space
-response=`curl -v -X POST https://${management}/api/spaces/${porg}/${catalog_space}/${space_travel}/publish \
+# If sending a zip instead of a WSDL, the type below should be application/zip
+response=`curl -v -X POST https://${management}/api/spaces/${porg}/${catalog_prod}/${space_travel}/publish \
                -s -k -H "Content-Type: multipart/form-data" -H "Accept: application/json" \
                -H "Authorization: Bearer ${provider_token}" \
-               -F "product=@math100-product.yaml" \
-               -F "openapi=@calculator100-api.yaml"`
+               -F "product=@math100-product.yaml;type=application/yaml" \
+               -F "openapi=@calculator100-api.yaml;type=application/yaml" \
+               -F "wsdl=@calculator100.wsdl;type=application/wsdl"`
 echo ${response} | jq .
-export provider_token=`echo ${response} | jq -r '.access_token'`
+echo ${response} | jq -r '.api_urls'
--- scripts/production-publish.sh
+++ scripts/production-publish.sh
@@ -6,16 +6,16 @@
 
 
 # Overview
-# - ...
+# - Publish a product that contains two OpenAPI definitions
 
 
 
 # Environment variables specific to this script
 export management=${management}
 export provider_idp=${provider_idp}
-export provider_username=${provider_username}
+export provider_username=steve-production
 export provider_password=${provider_password}
-export porg=${porg}
+export porg=acme-production
 export catalog_prod=${catalog_prod}
 export space_travel=${space_travel}
 
@@ -24,6 +24,7 @@ export space_travel=${space_travel}
 echo
 echo Create the Routes API
 rm -f routes100-api.yaml
+# apic create:api --name "routes" --version "1.0.0" --gateway-type "datapower-api-gateway" --title "Routes API" --filename routes100-api.yaml
 apic create:api --name "routes" --version "1.0.0" --title "Routes API" --filename routes100-api.yaml
 
 
@@ -31,14 +32,17 @@ apic create:api --name "routes" --version "1.0.0" --title "Routes API" --filenam
 echo
 echo Create the Trails API
 rm -f trails100-api.yaml
+# apic create:api --name "trails" --version "1.0.0" --gateway-type "datapower-api-gateway" --title "Trails API" --filename trails100-api.yaml
 apic create:api --name "trails" --version "1.0.0" --title "Trails API" --filename trails100-api.yaml
 
 
 
 echo
 echo Create the Climbon Product referencing the Routes and Trails APIs
-rm -f climbon100-product.yaml
-apic create:product --name "climbon" --version "1.0.0" --title "Climbon Product" --apis "routes100-api.yaml trails100-api.yaml" --filename climbon100-product.yaml
+# rm -f climbon100-product.yaml
+# apic create:product --name "climbon" --version "1.0.0" --gateway-type "datapower-api-gateway" --title "Climbon Product" --apis "routes100-api.yaml trails100-api.yaml" --filename climbon100-product.yaml
+# apic create:product --name "climbon" --version "1.0.0" --title "Climbon Product" --apis "routes100-api.yaml trails100-api.yaml" --filename climbon100-product.yaml
+# Once created, the $ref reference to the API files must be replace with name: api-name:api-version (see climbon100-product.yaml)
 
 
 
@@ -59,86 +63,11 @@ export provider_token=`echo ${response} | jq -r '.access_token'`
 
 echo
 echo Publish the Product to the Space
-response=`curl -X POST https://${management}/api/spaces/${porg}/${catalog_space}/${space_travel}/publish \
+response=`curl -X POST https://${management}/api/spaces/${porg}/${catalog_prod}/${space_travel}/publish \
                -s -k -H "Content-Type: multipart/form-data" -H "Accept: application/json" \
                -H "Authorization: Bearer ${provider_token}" \
-               -F "product=@climbon100-product.yaml" \
-               -F "openapi=@routes100-api.yaml" \
-               -F "openapi=@trails100-api.yaml"`
+               -F "product=@climbon100-product.yaml;type=application/yaml" \
+               -F "openapi=@routes100-api.yaml;type=application/yaml" \
+               -F "openapi=@trails100-api.yaml;type=application/yaml"`
 echo ${response} | jq .
-export provider_token=`echo ${response} | jq -r '.access_token'`
-
-
-
-echo
-echo Create the Consumer Organization Owner
-response=`curl`
-echo ${response} | jq .
-export provider_token=`echo ${response} | jq -r '.access_token'`
-
-
-
-echo
-echo Create the Consumer Organization
-response=`curl -X POST XXXXXXXXXXXXXXXX \
-               -s -k -H "Content-Type: application/json" -H "Accept: application/json" \
-               -H "Authorization: Bearer ${provider_token}" \
-               -d "{ \"name\": \"${corg}\",
-                     \"title\": \"${corg_title\" }"`
-echo ${response} | jq .
-export consumer_org=`echo ${response} | jq -r '.url'
-
-
-
-echo
-echo Create the Application
-response=`curl -X POST ${consumer_org}/apps \
-               -s -k -H "Content-Type: application/json" -H "Accept: application/json" \
-               -H "Authorization: Bearer ${provider_token}" \
-               -d "{ \"name\": \"${app}\",
-                     \"title\": \"${app_title\" }"`
-echo ${response} | jq .
-export provider_token=`echo ${response} | jq -r '.access_token'`
-
-
-
-echo
-echo Subscribe the Application to the Product
-response=`curl -X POST ${consumer_org}/apps \
-               -s -k -H "Content-Type: application/json" -H "Accept: application/json" \
-               -H "Authorization: Bearer ${provider_token}" \
-               -d "{ \"name\": \"${app}\",
-                     \"title\": \"${app_title\" }"`
-echo ${response} | jq .
-export provider_token=`echo ${response} | jq -r '.access_token'`
-
-
-
-echo
-echo Get Routes API URL to invoke
-response=`curl -X GET XXXX \
-               -s -k -H "Accept: application/json" \
-               -H "Authorization: Bearer ${provider_token}"
-echo ${response} | jq .
-export provider_token=`echo ${response} | jq -r '.access_token'`
-
-
-
-echo
-echo Get Trails API URL to invoke
-response=`curl -X GET XXXX \
-               -s -k -H "Accept: application/json" \
-               -H "Authorization: Bearer ${provider_token}"
-echo ${response} | jq .
-export provider_token=`echo ${response} | jq -r '.access_token'`
-
-
-
-echo
-echo Execute tests using the Subscription Credentials
-
-
-
-echo
-echo Delete the Catalog
-response=`curl -X DELETE XXXX`
+echo ${response} | jq -r '.api_urls'
